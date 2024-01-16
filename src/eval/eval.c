4d778e802a78d065a3a3a166a226043238db664b
Emmanuel <emmanuel.vouillon@epita.fr>
2024-01-16 23:32:17 +0100

    feat(eval): starts implem of eval_pipe

--- src/eval/eval.c
+++ src/eval/eval.c
@@ -17,6 +17,7 @@ static int eval_if(struct ast *ast);
 static int eval_cmd(struct ast *ast);
 static int eval_list(struct ast *ast);
 static int eval_not(struct ast *ast);
+static int eval_pipe(struct ast *ast);
 
 int eval(struct ast *ast)
 {
@@ -36,6 +37,10 @@ int eval(struct ast *ast)
     else if (ast->type == AST_NOT)
     {
         return eval_not(ast);
+    }
+    else if (ast->type == AST_PIPE)
+    {
+        return eval_pipe(ast);
     }
     else
     {
@@ -114,4 +119,79 @@ static int eval_list(struct ast *ast)
 static int eval_not(struct ast *ast)
 {
     return !eval(ast->children[0]);
+}
+
+static int exec_fork(char **argv, int *fds, enum side s)
+{
+    pid_t pid = fork();
+    if (pid != 0)
+    {
+        return pid;
+    }
+
+    if (s == LEFT)
+    {
+        dup2(fds[0], STDOUT_FILENO);
+
+        close(fds[0]);
+    }
+    else if (s == MID)
+    {
+        dup2(fds[0], STDIN_FILENO);
+        dup2(fds[1], STDOUT_FILENO);
+
+        close(fds[0]);
+        close(fds[1]);
+    }
+    else
+    {
+        dup2(fds[0], STDIN_FILENO);
+
+        close(fds[0]);
+    }
+
+    execvp(argv[0], argv);
+
+    errx(1, "execvp failed");
+}
+
+static int eval_pipe(struct ast *ast)
+{
+    int n = 10;
+
+    int *fds = malloc(2 * (n - 1) * sizeof(int));
+
+    int pid = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (pipe(fds + i * 2) == -1)
+        {
+            errx(1, "issue while piping");
+        }
+
+        enum side pos = i == n - 1 ? RIGHT : MID;
+
+        if (i == 0)
+        {
+            pid = exec_fork(ast->children[i]->args, 0, LEFT);
+        }
+        else
+        {
+            pid = exec_fork(ast->children[i]->args + i, fds + (i - 1) * 2 + 1,
+                            pos);
+        }
+    }
+
+    for (int i = 0; i < 2 * (n - 1); i++)
+    {
+        close(fds[i]);
+    }
+
+    free(fds);
+
+    int wstatus;
+    waitpid(pid, &wstatus, 0);
+
+    return WEXITSTATUS(wstatus);
 }
--- src/eval/eval.h
+++ src/eval/eval.h
@@ -3,6 +3,13 @@
 
 #include "../ast/ast.h"
 
+enum side
+{
+    LEFT,
+    RIGHT,
+    MID,
+};
+
 int eval(struct ast *ast);
 
 #endif /* !EVAL_H */
